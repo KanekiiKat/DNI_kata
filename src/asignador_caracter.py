6ec596cb86591013d5dfe05c8e1bfe487a4c3452
Pistacho14 <pablogonzaleztemes@gmail.com>
2025-01-23 13:21:45 +0100

    feat(src): :sparkles: añadir comprobador de letra dni
    Se añadió la comprobación de la letra del DNI.

---
+++ src/asignador_caracter.py
@@ -0,0 +1,15 @@
+class AsignadorCaracter:
+    
+    TABLA_ASIGNACIONES = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 
+                        'B', 'N', 'J','Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E']
+    
+    @classmethod
+    def get_tabla(cls):
+        return cls.TABLA_ASIGNACIONES
+    
+    @classmethod
+    def es_letra_correcta(cls, dni:str):
+        return cls.TABLA_ASIGNACIONES[ dni % (len(cls.TABLA_ASIGNACIONES) + 1)] == dni[-1]
+    
+    def __repr__(self):
+        return "".join(self.get_tabla())
