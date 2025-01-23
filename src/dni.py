fab5484b8705902481d505b5c0fd78e8ff4e93ea
Pistacho14 <pablogonzaleztemes@gmail.com>
2025-01-23 13:28:29 +0100

    feat(src): :sparkles: añadir constructor de objetos

---
+++ src/dni.py
@@ -0,0 +1,10 @@
+from src.asignador_caracter import  AsignadorCaracter
+
+class Dni:
+    
+    def __init__(self, dni):
+        
+        self.dni = dni
+        
+    def getDni(self):
+        return self.dni
